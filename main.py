6b581e7e04b6a8d31b883a6026398b1b703def07
Rodevp <rodriguezrodrigor12@gmail.com>
2022-12-07 18:39:07 -0500

    test

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import qrcode as qr
+from PIL import Image
 
 
 
 def main() :
     
-    img = qr.make("https://www.youtube.com/watch?v=bi2NLbLYbic")
+    towards = "https://www.youtube.com/watch?v=bi2NLbLYbic"
+
+    code = qr.QRCode(version=1, error_correction=qr.ERROR_CORRECT_H, box_size=10, border=4)
+    code.add_data(towards)
+    code.make(fit=True)
     
+    img = code.make_image()
     img.save("example.png")
-
-
 
 
 if __name__ == "__main__" :
